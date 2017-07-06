cbe6dabbd35852840e92df4a703e36a06ea86b61
Justus Schriedel <justusschriedel@gmail.com>
2017-07-05 21:37:46 -0400

    added hello world app

---
+++ HelloWorld2/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //String hello = getResources().getString(R.string.hello_world);
+
+        //TextView textView = new TextView(this);
+        //textView.setText(R.string.hello_world);
+    }
+}
