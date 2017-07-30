2ef2fa90edf7ecf531354554ee407aeedbc4254e
Justus Schriedel <justusschriedel@gmail.com>
2017-07-30 12:43:39 -0400

    nothing added

--- project2/TaskOne.java
+++ project2/TaskOne.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.nio.*;
 
 public class TaskOne {
-    public int fileSize, totalPackets, totalIP, totalTCP, totalUDP, totalConn;
+    public static int fileSize, totalPackets, totalIP, totalTCP, totalUDP, totalConn;
     
     public static void main(String[] args) {
 	FileInputStream fromFile;
@@ -29,9 +29,30 @@ public class TaskOne {
 	    e.printStackTrace();
 	}
 
-	ByteBuffer bytes = ByteBuffer.wrap(fileBytes);
+	//System.out.println(fileBytes[36] + " " + fileBytes[37] + " "
+	//		   + fileBytes[38] + " " + fileBytes[39]);
 
-	System.out.println(bytes.getInt(36));
+	ByteBuffer bytes = ByteBuffer.wrap(fileBytes);
+	IntBuffer fileInfo = bytes.asIntBuffer();
+
+	System.out.println(fileInfo.get(36));
+
+	totalPackets = 0;
+	totalIP = 0;
+	totalTCP = 0;
+	totalUDP = 0;
+	totalConn = 0;
+
+	/*int i = 36; int x = 0;
+	while (i < fileBytes.length && x < 45) {
+	    System.out.println(i);
+	    totalPackets++;
+	    int size = fileBytes[i];
+	    i += size + 12;
+	    x++;
+	}
+
+	System.out.println(totalPackets);*/
     }
 	
 }
