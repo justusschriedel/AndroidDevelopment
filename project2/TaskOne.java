6781a139377890860d79a3ac720ce303d375d8e0
Justus Schriedel <justusschriedel@gmail.com>
2017-07-31 17:35:47 -0400

    made progress on task one

--- project2/TaskOne.java
+++ project2/TaskOne.java
@@ -6,14 +6,14 @@ public class TaskOne {
     public static int fileSize, totalPackets, totalIP, totalTCP, totalUDP, totalConn;
     
     public static void main(String[] args) {
-	FileInputStream fromFile;
+	//FileInputStream fromFile;
 	Scanner scan = new Scanner(System.in);
-	byte[] fileBytes;
+	//byte[] fileBytes;
 	//ByteBuffer bytes;
 
 	System.out.println("Enter .pcap file name:");
 	String fileName = scan.nextLine();
-	File file = new File(fileName);
+	/*File file = new File(fileName);
 	int fileSize = Math.toIntExact(file.length());
 
 	fileBytes = new byte[fileSize];
@@ -43,6 +43,8 @@ public class TaskOne {
 	totalUDP = 0;
 	totalConn = 0;
 
+	System.out.println(bytes.order(ByteOrder.LITTLE_ENDIAN).getInt(402));
+	
 	/*int i = 36; int x = 0;
 	while (i < fileBytes.length && x < 45) {
 	    System.out.println(i);
@@ -53,6 +55,44 @@ public class TaskOne {
 	}
 
 	System.out.println(totalPackets);*/
-    }
+
+        Runtime runtime = Runtime.getRuntime();
+	BufferedReader infoReader = null;
+
+	String[] ips = null;
+	String[] tcps = null;
+	String[] udps = null;
 	
+	try {
+	    Process process = runtime.exec("tshark -r " + fileName + " -z ptype,tree");
+	    infoReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+	    totalPackets = 0;
+	    totalIP = 0;
+	    totalTCP = 0;
+	    totalUDP = 0;
+	    totalConn = 0;
+	    
+	    String line = infoReader.readLine();
+	    while (!(line.contains("======="))) {
+	        totalPackets++;
+		line = infoReader.readLine();
+	    }
+
+	    while (!(line.startsWith("IP Protocol Types "))) {
+		line = infoReader.readLine();
+	    }
+
+	    ips = line.split(" ");
+	    tcps = infoReader.readLine().split(" ");
+	    udps = infoReader.readLine().split(" ");
+	}
+	catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+	// TODO: write method that rids ips, tcps, udps of elements that are just spaces
+
+	System.out.println((--totalPackets) + " " + ips[3] + " " + tcps[1] + " " + udps[1]);
+    }
 }
