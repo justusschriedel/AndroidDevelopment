291014f7dc3718bf384ef3d80823455946733fd2
Justus Schriedel <justusschriedel@gmail.com>
2017-08-01 17:50:44 -0400

    took out tshark usage

--- project2/TaskOne.java
+++ project2/TaskOne.java
@@ -3,17 +3,19 @@ import java.io.*;
 import java.nio.*;
 
 public class TaskOne {
-    public static int fileSize, totalPackets, totalIP, totalTCP, totalUDP, totalConn;
+    
     
     public static void main(String[] args) {
-	//FileInputStream fromFile;
+	int fileSize, totalPackets, totalIP, totalTCP, totalUDP, totalConn;
+	FileInputStream fromFile;
 	Scanner scan = new Scanner(System.in);
-	//byte[] fileBytes;
-	//ByteBuffer bytes;
+	byte[] fileBytes;
+	ByteBuffer bytes;
 
 	System.out.println("Enter .pcap file name:");
 	String fileName = scan.nextLine();
-	/*File file = new File(fileName);
+
+	File file = new File(fileName);
 	int fileSize = Math.toIntExact(file.length());
 
 	fileBytes = new byte[fileSize];
@@ -29,9 +31,6 @@ public class TaskOne {
 	    e.printStackTrace();
 	}
 
-	//System.out.println(fileBytes[36] + " " + fileBytes[37] + " "
-	//		   + fileBytes[38] + " " + fileBytes[39]);
-
 	ByteBuffer bytes = ByteBuffer.wrap(fileBytes);
 	IntBuffer fileInfo = bytes.asIntBuffer();
 
@@ -45,7 +44,7 @@ public class TaskOne {
 
 	System.out.println(bytes.order(ByteOrder.LITTLE_ENDIAN).getInt(402));
 	
-	/*int i = 36; int x = 0;
+	int i = 36; int x = 0;
 	while (i < fileBytes.length && x < 45) {
 	    System.out.println(i);
 	    totalPackets++;
@@ -54,8 +53,11 @@ public class TaskOne {
 	    x++;
 	}
 
-	System.out.println(totalPackets);*/
+	System.out.println(totalPackets)
 
+    }
+}
+	/*	
         Runtime runtime = Runtime.getRuntime();
 	BufferedReader infoReader = null;
 
@@ -68,10 +70,6 @@ public class TaskOne {
 	    infoReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
 	    totalPackets = 0;
-	    totalIP = 0;
-	    totalTCP = 0;
-	    totalUDP = 0;
-	    totalConn = 0;
 	    
 	    String line = infoReader.readLine();
 	    while (!(line.contains("======="))) {
@@ -91,8 +89,45 @@ public class TaskOne {
 	    e.printStackTrace();
 	}
 
-	// TODO: write method that rids ips, tcps, udps of elements that are just spaces
+	try {
+	    Process process = runtime.exec("tshark -r " + fileName + " -z conv,tcp");
+	    infoReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-	System.out.println((--totalPackets) + " " + ips[3] + " " + tcps[1] + " " + udps[1]);
-    }
-}
+	    totalConn = 0;
+	    
+	    String line = infoReader.readLine();
+	    while (!(line.contains("======="))) {
+		line = infoReader.readLine();
+	    }
+
+	    int i = 0;
+	    while (i < 5) {
+		line = infoReader.readLine();
+		i++;
+	    }
+	    
+	    while (!(line.contains("======="))) {
+		totalConn++;
+		line = infoReader.readLine();
+	    }
+	}
+	catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+	System.out.println((--totalPackets) + " " + removeSpaces(ips).get(3) + " " +
+			   removeSpaces(tcps).get(1) + " " + removeSpaces(udps).get(1) +
+			   " " + totalConn + "\n");
+
+
+    public static ArrayList<String> removeSpaces(String[] array) {
+	ArrayList<String> newArray = new ArrayList<String>();
+
+	for (int i = 0; i < array.length; i++) {
+	    if (!(array[i].equals(""))) {
+		newArray.add(array[i]);
+	    }
+	}
+
+	return newArray;
+    }*/
